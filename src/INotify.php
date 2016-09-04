64ca6c168e9533d4b837dc985d53d9dfa51984bb
Ronmi Ren <ronmi.ren@gmail.com>
2016-09-04 17:05:21 +0800

    abstracts access to inotify of pecl, so we can write test cases for our primary logic easier

---
+++ src/INotify.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Fruit\WatchKit;
+
+interface Inotify {
+    function add($path, $mode); // will return a resource, it MUST be blocking default
+    function del($resource);
+    function len();
+    function read(); // this is non-blocking
+    function wait(); // this is blocking read
+    function resource(); // return stream resource, so we can use stream_select
+}
---
+++ src/PECLInotify.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Fruit\WatchKit;
+
+class PECLInotify implements Inotify
+{
+    protected $inotify;
+    public function __contruct()
+    {
+        $this->inotify = inotify_init();
+        stream_set_blocking($this->inotify, true);
+    }
+
+    public function add($path, $mode)
+    {
+        return inotify_add_watch($this->inotify, $path, $mode);
+    }
+
+    public function del($resource)
+    {
+        return inotify_rm_watch($this->inotify, $resource);
+    }
+
+    public function len()
+    {
+        return inotify_queue_len($this->inotify);
+    }
+
+    public function read()
+    {
+        // should just call $this->len(), but copy-paste
+        // here to reduce one function call since the logic
+        // in len() is simple
+        if (inotify_queue_len($this->inotify) < 1) {
+            return false;
+        }
+
+        // should just call $this->wait(), but copy-paste
+        // here to reduce one function call since the logic
+        // in wait() is simple
+        return inotify_read($this->inotify);
+    }
+
+    public function wait()
+    {
+        return inotify_read($this->inotify);
+    }
+
+    public function resource()
+    {
+        return $h;
+    }
+}
